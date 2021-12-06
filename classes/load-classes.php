9de4708c910bbd744b2a348376d6dd745aca76d8
wpxhouston <madmak787@gmail.com>
2021-12-06 17:58:34 +0530

    Load all the required classes.
    Load all the required classes.

---
+++ classes/load-classes.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The bootstrap classes
+ *
+ * @link       http://khanamir.me/
+ * @since      1.0.0
+ *
+ * @package    Madmak
+ * @subpackage Madmak/classes
+ */
+
+/**
+ * Load list of classes
+ */
+require_once 'class-rtcamp-admin.php';
+require_once 'class-rtcamp-front.php';
