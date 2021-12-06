b8e7c7de71461b43133944317a64247a7d997884
wpxhouston <madmak787@gmail.com>
2021-12-06 18:02:34 +0530

    PHPunit test files
    PHPunit test files

---
+++ tests/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Post_Contributers
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', 'C:/phpunit/phpunit-polyfills/phpunitpolyfills-autoload.php' );
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/post-contributers.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
